456df8ff932b799c0cd3d0cf8deb54972d05d8d3
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-03-05 21:09:08 +0300

    Person

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        Person person1 = new Person("John", "Doe", 20);
+        Person person = new Person("John", "Doe", 20);
+        System.out.println("Firstname: " + person.getFirstName());
+        System.out.println("LastName: " + person.getLastName());
+        System.out.println("Age: " + person.getAge());
+
+        Person person1=new Person("feyza", "Türkay",44,"Kadın",170,"fturkay@gmail");
         System.out.println("Firstname: " + person1.getFirstName());
         System.out.println("LastName: " + person1.getLastName());
         System.out.println("Age: " + person1.getAge());
 
-        Person person2=new Person("feyza", "Türkay",44,"Kadın",170,"fturkay@gmail");
-        System.out.println("Firstname: " + person2.getFirstName());
-        System.out.println("LastName: " + person2.getLastName());
-        System.out.println("Age: " + person2.getAge());
+        Wall wall=new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
     }
-
 
 }
